70d7154dc61c27d50318045e4ce863665a7ca9e6
davidsilva131 <davidsilvac131@gmail.com>
2023-04-22 18:06:32 -0500

    Update sorteo page, no use Loading component

--- components/Loading.jsx
+++ components/Loading.jsx
@@ -4,7 +4,7 @@ const Loading = () => {
       <div role='status'>
         <svg
           aria-hidden='true'
-          className='inline w-8 h-8 mr-2 text-gray-200 animate-spin dark:text-gray-600 fire-gradient'
+          className='inline w-20 h-20 mr-2 text-gray-200 animate-spin fill-blue-700'
           viewBox='0 0 100 101'
           fill='none'
           xmlns='http://www.w3.org/2000/svg'
